6c5a4cec8b54642b87a678bc3a8a7ce8933d3dd5
kike <mkike99999@gmail.com>
2025-04-05 20:05:16 -0600

    sesion1

--- 3.1.1/script.js
+++ 3.1.1/script.js
@@ -1,1 +1,59 @@
 console.log("Hola Mundo");
+
+
+
+
+let nombre = "Enrique";
+console.log(nombre);
+
+nombre = "Daniel"
+console.log(nombre);
+
+
+const GRAVEDAD = 9.81;
+const PI = 3.1416;
+
+let edad = 19;
+let estatura = 1.92;
+let gradosCentrigrados = -20;
+
+console.log(edad);
+console.log(estatura);
+console.log(gradosCentrigrados);
+
+
+let mensajeFinal = nombre + " tiene " + edad + " años";
+console.log(mensajeFinal);
+
+let MayorDeEdad = true;
+let TerceraEdad = false;
+
+alert(mensajeFinal);
+let nombreMascota = prompt("Dime el nombre de tu mascota");
+alert("Tu mascota se llama " + nombreMascota);
+
+//leer los datos de una mascota e imprimir un mensaje que incluya todos los datos leidos
+// utilizar al menos 1 vez cada tipo de dato (string, number y Boolean)
+// Ejercicio1
+
+let tiempoMascota = prompt("¿Cuantos años tienes con tu mascota?");
+let tipoMascota = prompt("Tu mascota es un perro?");
+
+alert("Tu mascota  " + tipoMascota + "  es un perro " + " tienes con ella " + tiempoMascota + " años y se llama " + nombreMascota);
+alert("Comienza el Ejercicio 2");
+
+
+//Ejercicio 2
+let pregunta1 = prompt("En que año naciste?");
+let pregunta2 = prompt("Como te llamas?");
+let pregunta3 = prompt("donde vives?");
+let pregunta4 = prompt("Cuantos años tienes?");
+let pregunta5 = prompt("Cual es tu comida favorita?");  
+let pregunta6 = prompt("Que prefieres, el mar o la montaña?");
+let pregunta7 = prompt("Que prefieres, el verano o el invierno?");
+let pregunta8 = prompt("Cual es tu deporte favorito?");
+let pregunta9 = prompt("Cual es tu color favorito?");
+let pregunta10 = prompt("Cual es tu animal favorito?");
+
+alert( "Tus respuestas fueron " + pregunta1 + ", " + pregunta2 + ", " + pregunta3 + ", " + pregunta5 + ", " + pregunta6 + ", " + pregunta7 + ", " + pregunta8 + ", " + pregunta9 + ", " + pregunta10);
+
