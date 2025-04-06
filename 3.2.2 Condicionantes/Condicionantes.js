1586b1aca30f5261c1be533ea028d8fab6c41664
kike <mkike99999@gmail.com>
2025-04-06 17:34:58 -0600

    sesion2

---
+++ 3.2.2 Condicionantes/Condicionantes.js
@@ -0,0 +1,23 @@
+
+alert("Hola mundo");
+
+let edad = 18;
+let traeine = true;
+let deseo = "batman";
+
+// no es necesario comparar booleanos (!TraeIne == no trae ine)
+edad >= 18 && traeine
+
+if(edad >= 18 && traeine){
+    alert("Puedes entrar al cine");
+    if( deseo == "Spiderman"){
+        alert("Veras tu pelicula deseada");
+    }
+    else{
+        alert("Veras otra pelicula");
+    }
+else if(edad >= 18 && !traeine){
+    alert("Muestrame una foto de tu Ine para entrar")
+} else{
+    alert("No puedes entrar al cine");
+}
